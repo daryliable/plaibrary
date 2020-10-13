8874b801d0e90e01394b5e4c525c5004f532da89
daryliable <cdongayo@gmail.com>
2020-10-14 05:52:58 +0800

    a

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
