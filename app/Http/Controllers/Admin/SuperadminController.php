28dbe6690ec13788fefc72d66d4fe18ca8580c21
daryliable <cdongayo@gmail.com>
2020-11-03 14:10:25 +0800

    a

--- app/Http/Controllers/Admin/SuperadminController.php
+++ app/Http/Controllers/Admin/SuperadminController.php
@@ -22,16 +22,21 @@ public function index()
         $noOfRequest = User::where('approved', '!=' , 1)->count();
         $noOfUploads = Book::count();
         
-    
+        $reserve = Reservation::all()->pluck('id');
+      
         $students = User::select(DB::raw("COUNT(*) as count"))
+                        ->whereRoleIs('student')
+                        ->where('approved', '=', 1)
                         ->whereYear('created_at',date('Y'))
                         ->groupBy(DB::raw("Month(created_at)"))
                         ->pluck('count');
         $months = User::select(DB::raw("Month(created_at) as month"))
+                        ->whereRoleIs('student')
+                        ->where('approved', '=', 1)
                         ->whereYear('created_at',date('Y'))
                         ->groupBy(DB::raw("Month(created_at)"))
                         ->pluck('month');
-      
+
         $datas = array(0,0,0,0,0,0,0,0,0,0,0,0,0);
         
         foreach($months as $index => $month)
