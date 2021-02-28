be485ed66d80987c092d282257566c585cbc03bb
daryliable <cdongayo@gmail.com>
2021-02-28 11:27:09 +0800

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use ConsoleTVs\Charts\Registrar as Charts;
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLength(191);
+    
         if (env('REDIRECT_HTTPS')) {
             $this->app['request']->server->set('HTTPS', true);
         }
@@ -25,11 +25,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(Charts $charts)
+    public function boot(Charts $charts, UrlGenerator $url)
     {  
         if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https');
         }
+
         Schema::defaultstringLength(191);
         $charts->register([
             \App\Charts\UserStatChart::class
