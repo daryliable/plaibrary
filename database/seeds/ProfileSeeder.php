70a6b5f8ae3b8829033d6c5105aae53076aa733c
daryliable <cdongayo@gmail.com>
2021-03-03 14:50:57 +0800

    up

--- database/seeds/ProfileSeeder.php
+++ database/seeds/ProfileSeeder.php
@@ -16,16 +16,19 @@ class ProfileSeeder extends Seeder
             array (
             'id'          => 1,
             'user_id'     => 1,
+            'designation'     =>'test1',
          ),
              1 => 
             array (
             'id'          => 2,
             'user_id'     => 2,
+            'designation'     =>'test2',
          ),
              2 => 
             array (
             'id'          => 3,
             'user_id'     => 3,
+            'designation'     =>'test3',
          ),
         ));
     }
