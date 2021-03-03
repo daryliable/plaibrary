36c005f0e377dc37e4d193b5f614b4fd8b0e51df
daryliable <cdongayo@gmail.com>
2021-03-03 15:46:36 +0800

    aa

--- app/User.php
+++ app/User.php
@@ -39,15 +39,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ]; 
-      protected static function boot()
-    {
-        parent::boot();
-
-        static::created(function ($user){
-            $user->profile()->create([
-            ]);
-     });
-    }
+     
     
     public function profile(){
     return $this->hasOne(Profile::class);
