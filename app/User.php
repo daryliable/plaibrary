d84324648442e397949fb6f44f29656a0adc01c7
daryliable <cdongayo@gmail.com>
2021-03-03 18:15:23 +0800

    asda

--- app/User.php
+++ app/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
 
     
     public function profile(){
-    return $this->hasOne(Profile::class);
+    return $this->hasOne(Profile::class, 'id', 'user_id');
     }
     public function books(){
     return $this->hasMany(Book::class);
