da7d1c03cd81cf24170bb66ef466be665426e95e
daryliable <cdongayo@gmail.com>
2021-03-04 09:36:00 +0800

    asda

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','roles','civil','user_id','coll_univ','contact_num','address',
+        'name', 'email', 'password','role_user','role_id','user_type','civil','user_id','coll_univ','contact_num','address',
     ];
 
     /**
