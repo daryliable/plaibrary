7712c36870d28d4a97df01089f44f31626aba2b1
daryliable <cdongayo@gmail.com>
2021-03-06 13:37:18 +0800

    aaaaaaaaaaa

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => $host,
+             'host' => $host,
             'port' => env('DB_PORT', '3306'),
             'database' => $database,
             'username' => $username,
