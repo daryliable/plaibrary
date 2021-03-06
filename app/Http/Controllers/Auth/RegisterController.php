c59374231d67f822b629f824115d183cf6fad2d4
daryliable <cdongayo@gmail.com>
2021-03-06 11:32:58 +0800

    gg

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
 
         $roles = $data['roles'];
         $user->save();
-        $user->attachRoles(explode(',', $roles));
+        $user->attachRole($roles);
         return $user;
     }
     
