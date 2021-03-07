db4acfc3281964cec3071ec1e79f153819543285
daryliable <cdongayo@gmail.com>
2021-03-07 19:33:33 +0800

    aa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,7 +82,9 @@
               
                   <select  style="margin-top: 15px" class="form-control" name="roles">
                     @foreach($roles as $role )
+                    @if($role<1)
                     <option value="{{$role->id}}">{{$role->name}}</option>
+                    @endif
                     @endforeach
                   </select>  
                 
